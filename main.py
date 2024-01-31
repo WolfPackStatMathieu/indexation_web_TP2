9fef7c36275608ab818a920fbdf64eec95ced912
Mathieu <mathieu.thomassin@eleve.ensai.fr>
2024-01-31 11:20:47 +0100

    modif sur l'index

--- main.py
+++ main.py
@@ -3,59 +3,60 @@ from nltk.tokenize import word_tokenize
 from nltk.stem import PorterStemmer
 from collections import defaultdict
 
-# Charger la liste d'URLs à partir du fichier JSON
+# Charger la liste de documents à partir du fichier JSON
 with open('crawled_urls_test.json', 'r', encoding='utf-8') as file:
-    data = json.load(file)
+    documents = json.load(file)
 
-# Initialiser les variables pour les statistiques
-num_documents = len(data)
-num_tokens_global = 0
-num_tokens_title = 0
-# num_tokens_content = 0
-num_tokens_h1 = 0
-average_tokens_per_document = 0
+# Initialiser l'index vide
 index = defaultdict(list)
 
 # Initialiser le stemmer de Porter
 porter_stemmer = PorterStemmer()
 
+# Initialiser les variables pour les statistiques
+num_documents = len(documents)
+num_tokens_global = 0
+average_tokens_per_document = 0
+
 # Parcourir chaque document dans la liste
-for document in data:
+for i, document in enumerate(documents):
     # Extraire les informations nécessaires
     url = document['url']
     title = document['title']
-    # content = document['content']
-    # h1 = document['h1']
+    content = document['content']
+    h1 = document['h1']
 
-    # Tokenization et stemming des titres, du contenu et de h1
-    tokens_title = [porter_stemmer.stem(token.lower()) for token in word_tokenize(title)]
-    # tokens_content = [porter_stemmer.stem(token.lower()) for token in word_tokenize(content)]
-    # tokens_h1 = [porter_stemmer.stem(token.lower()) for token in word_tokenize(h1)]
+    # Tokenization des champs url, title, content, h1
+    tokens_url = word_tokenize(url.lower())
+    tokens_title = word_tokenize(title.lower())
+    tokens_content = word_tokenize(content.lower())
+    tokens_h1 = word_tokenize(h1.lower())
 
-    # Mise à jour des statistiques globales
-    num_tokens_global += len(tokens_title) #+ len(tokens_content) + len(tokens_h1)
-    num_tokens_title += len(tokens_title)
-    # num_tokens_content += len(tokens_content)
-    # num_tokens_h1 += len(tokens_h1)
+    # Optionnel: Traitement de chaque token (ex: stemming)
+    processed_tokens_url = [porter_stemmer.stem(token) for token in tokens_url]
+    processed_tokens_title = [porter_stemmer.stem(token) for token in tokens_title]
+    processed_tokens_content = [porter_stemmer.stem(token) for token in tokens_content]
+    processed_tokens_h1 = [porter_stemmer.stem(token) for token in tokens_h1]
 
-    # Mise à jour de l'index
-    for token in tokens_title:# + tokens_content + tokens_h1:
-        index[token].append(url)
+    # Construction de l'index inversé pour chaque champ
+    tokens_all = processed_tokens_url + processed_tokens_title + processed_tokens_content + processed_tokens_h1
+    num_tokens_global += len(tokens_all)
+    
+    # Créer une liste inversée pour chaque token
+    for token in set(tokens_all):
+        index[token].append(i)
 
 # Calcul des statistiques finales
 average_tokens_per_document = num_tokens_global / num_documents
 
 # Enregistrement de l'index dans un fichier JSON
-with open('title_non_pos_index.json', 'w', encoding='utf-8') as index_file:
+with open('index.json', 'w', encoding='utf-8') as index_file:
     json.dump(index, index_file, ensure_ascii=False, indent=2)
 
 # Enregistrement des statistiques dans un fichier JSON
 metadata = {
     'num_documents': num_documents,
     'num_tokens_global': num_tokens_global,
-    'num_tokens_title': num_tokens_title,
-    # 'num_tokens_content': num_tokens_content,
-    # 'num_tokens_h1': num_tokens_h1,
     'average_tokens_per_document': average_tokens_per_document
 }
 
